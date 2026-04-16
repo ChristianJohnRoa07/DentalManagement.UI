1db37751d6697d6eb93c0ab3a297fcc48adb2a9a
Christian John Roa <roacj.cr@gmail.com>
2026-04-16 16:18:12 +0800

    Implement theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { ThemeProvider, createTheme, CssBaseline } from '@mui/material'
+import { ThemeProvider, CssBaseline } from '@mui/material'
 import './index.css'
 import App from './App.tsx'
-
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#1976d2',
-    },
-  },
-})
+import { theme } from '../src/common/theme.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
